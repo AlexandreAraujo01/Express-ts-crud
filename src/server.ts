804712ad7269a4aea4f8f921333e573caa8b8332
Alex <alexandre.roberto115@gmail.com>
2025-07-19 09:24:43 -0300

    permitindo apenas conexoes via frontend

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,20 @@ import express, { Request, Response } from 'express';
 import { jogosRouter } from './http/routes/jogos-router';
 import path from 'node:path';
 import { pagesRouter } from './http/routes/pages-router';
+import cors from 'cors';
 
 
 const app = express();
 app.use(express.json())
-app.use('/api', jogosRouter)
 
+const allowedOrigin = 'http://localhost:3000';
+
+app.use(cors({
+  origin: allowedOrigin
+}));
+
+//Routes
+app.use('/api', jogosRouter)
 app.use('/', pagesRouter)
 
 app.get('/health', (req: Request, res: Response) => {
